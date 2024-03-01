ff4d8fa8eb7bf262208d12f840c84d587088ddef
Emiliano Cancedda <E.Cancedda@almaviva.it>
2024-03-01 12:25:57 +0100

    Venerdi 01/03/2024

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -3,16 +3,24 @@ using System.Runtime.ExceptionServices;
 
 int? zero = 0;
 
+//TRY CATCH FINALLY
 
 try
 {
+	//PRENDIAMO IN INPUT DUE NUMERI
+
 	Console.WriteLine("Inserisci il primo numero");
+	//USIAMO IL METODO INT.PARSE PER FARE UNA CONVERSIONE O CAST ESPLICITO DELLA VARIBILE POICHE Console.ReadLine() RESTITUISCE UNA STRINGA  
 	int? a = int.Parse(Console.ReadLine());
+
 	Console.WriteLine("Inserisci il secondo numero");
 	int? b = int.Parse(Console.ReadLine());
 
+	//CREIAMO UN ECCEZIONE
+
 	zero = a / b;
 
+	//DA QUI GESTIAMO ALCUNE POSSIBILI ECCEZIONI
 }catch (DivideByZeroException VariabileDiErrore){
 	Console.WriteLine("Error 1: "+VariabileDiErrore);
 }catch (DuplicateWaitObjectException VariabileDiErrore){
@@ -24,20 +32,24 @@ try
 }catch (FormatException VariabileDiErrore){
 	Console.WriteLine("Error 5: " + VariabileDiErrore);
 	try { 
+		//GENERATORE FAI FA TE DI ECCEZIONI
 		makeMock(5);
 	}
+	//ULTERIORE GESTIONE GENERICA DELL'ECCEZIONE
 	catch(Exception ex) { 
 		Console.WriteLine(ex.Message);
 	}
-
 }catch (NotImplementedException VariabileDiErrore){
 	Console.WriteLine("Error 6: " + VariabileDiErrore);
 }catch (Exception VariabileDiErrore){
 	Console.WriteLine("Error 7 ( Generic ): " + VariabileDiErrore);
-}finally { 
+}
+//QUESTO BLOCCO VIENE ESEGUITO A PRESCINDERE DALLA PRESENZA O MENO DEGLI ERRORI
+finally { 
 	Console.WriteLine("Tutte le eventuali eccezioni sono state gestite [ Forse ;-) ]");
 }
 
+//FUNZIONE FAI DA TE PER LA GENERAZIONE DI ALCUNI ERRORI
 void makeMock(int n){
 	switch (n){
 		case 0:
