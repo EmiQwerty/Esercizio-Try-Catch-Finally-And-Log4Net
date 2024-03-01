bc53a4f3b5ed7437cbce62f50b43c35f9a2613c0
Emiliano Cancedda <E.Cancedda@almaviva.it>
2024-03-01 15:39:05 +0100

    Configurazione finale di log4net.config e mapping dei colori

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System.Data.SqlTypes;
+using System.IO;
+using System.Reflection;
 using System.Runtime.ExceptionServices;
+using log4net;
+using log4net.Config;
 
 int? zero = 0;
 
-//TRY CATCH FINALLY
+//DICHIARIAMO E INIZIALIZZIAMO 
 
+var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+var _log4net = LogManager.GetLogger(typeof(Program));
+
+//TRY CATCH FINALLY
 try
 {
 	//PRENDIAMO IN INPUT DUE NUMERI
+	_log4net.Info("Start Program");
 
 	Console.WriteLine("Inserisci il primo numero");
 	//USIAMO IL METODO INT.PARSE PER FARE UNA CONVERSIONE O CAST ESPLICITO DELLA VARIBILE POICHE Console.ReadLine() RESTITUISCE UNA STRINGA  
@@ -21,28 +32,13 @@ try
 	zero = a / b;
 
 	//DA QUI GESTIAMO ALCUNE POSSIBILI ECCEZIONI
-}catch (DivideByZeroException VariabileDiErrore){
-	Console.WriteLine("Error 1: "+VariabileDiErrore);
-}catch (DuplicateWaitObjectException VariabileDiErrore){
-	Console.WriteLine("Error 2: " + VariabileDiErrore);
-}catch (ArithmeticException VariabileDiErrore){
-	Console.WriteLine("Error 3: " + VariabileDiErrore);
-}catch (InvalidOperationException VariabileDiErrore){
-	Console.WriteLine("Error 4: " + VariabileDiErrore);
-}catch (FormatException VariabileDiErrore){
-	Console.WriteLine("Error 5: " + VariabileDiErrore);
-	try { 
-		//GENERATORE FAI FA TE DI ECCEZIONI
-		makeMock(5);
-	}
-	//ULTERIORE GESTIONE GENERICA DELL'ECCEZIONE
-	catch(Exception ex) { 
-		Console.WriteLine(ex.Message);
-	}
+}
+catch (DivideByZeroException VariabileDiErrore){
+	_log4net.Error("Error Type: "+ VariabileDiErrore);
 }catch (NotImplementedException VariabileDiErrore){
-	Console.WriteLine("Error 6: " + VariabileDiErrore);
+	_log4net.Error("Error Type: " + VariabileDiErrore);
 }catch (Exception VariabileDiErrore){
-	Console.WriteLine("Error 7 ( Generic ): " + VariabileDiErrore);
+	_log4net.Error("Error Type: " + VariabileDiErrore);
 }
 //QUESTO BLOCCO VIENE ESEGUITO A PRESCINDERE DALLA PRESENZA O MENO DEGLI ERRORI
 finally { 
